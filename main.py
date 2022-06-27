89945488622e67dd849db4b7c482fbaeb00bac71
Anastasia <anastasia@plrx-00009957.local>
2022-06-27 21:40:56 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,130 @@
+import requests
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from wb import search_data, get_card_info, prepare_card_info, info_to_upload
+
+# state options
+ENTER_API_KEY = 1
+ENTER_NM_ID = 2
+ENTER_NEW_NAME = 3
+
+
+# function to handle the /start command
+def start(update, context):
+    context.user_data['state'] = ENTER_API_KEY
+    update.message.reply_text("Для начала работы с ботом предоставьте API ключ")
+
+
+def received_api_key(update, context):
+    api_key = update.message.text
+    try:
+        info = requests.get('https://suppliers-api.wildberries.ru/api/v1/directory/wbsizes',
+                            headers={"Authorization": api_key})
+
+        if info.status_code != 200:
+            update.message.reply_text("Что-то пошло не так..." + str(info.status_code))
+            update.message.reply_text("Статус" + str(info.status_code))
+            if info.status_code == 401:
+                update.message.reply_text("Введён неверный API ключ")
+            update.message.reply_text("Попробуйте ещё раз")
+            raise ValueError("invalid value")
+        else:
+            context.user_data['api_key'] = api_key
+            context.user_data['state'] = ENTER_NM_ID
+            update.message.reply_text("Верный API ключ")
+            update.message.reply_text("Введите номенклатуру товара, у которого хотите изменить наименование")
+    except:
+        update.message.reply_text("Что-то пошло не так 1...")
+
+
+def received_nm_id(update, context):
+    try:
+        nm_id = int(update.message.text)
+    except:
+        update.message.reply_text("Номенклатура должна состоять только из цифр. Попробуйте ещё раз.")
+        raise ValueError("invalid value")
+    data = search_data(nm_id)
+    try:
+        info = get_card_info(data=data, headers={"Authorization": context.user_data['api_key']})
+        update.message.reply_text("Информация о товаре загружена. Статус " + str(info.status_code))
+        if info.status_code != 200:
+            update.message.reply_text("Что-то пошло не так 2...")
+            if info.status_code == 401:
+                update.message.reply_text("Номенклатура не найдена")
+            update.message.reply_text("Попробуйте ещё раз")
+        else:
+            info = prepare_card_info(info=info, headers={"Authorization": context.user_data['api_key']})
+            context.user_data['card_info'] = info
+            context.user_data['state'] = ENTER_NEW_NAME
+            update.message.reply_text("Текущее наименование товара: {}".format(info['card']['addin'][1]['params'][0]['value']))
+            update.message.reply_text("Введите новое наименование товара. После отправки сообщения наименование будет изменено.")
+    except:
+        update.message.reply_text("Что-то пошло не так 22...")
+
+
+def received_new_name(update, context):
+    new_name = str(update.message.text)
+    try:
+        info = info_to_upload(context.user_data['card_info'], new_name)
+        updated_info = requests.post('https://suppliers-api.wildberries.ru/card/update', json=info, headers={"Authorization": context.user_data['api_key']})
+        if updated_info.status_code != 200:
+            update.message.reply_text("Что-то пошло не так...")
+            update.message.reply_text("Попробуйте ещё раз")
+            raise ValueError("invalid value")
+        else:
+            context.user_data['state'] = ENTER_NM_ID
+            update.message.reply_text("Наименование успешно изменено")
+            update.message.reply_text("Введите номенклатуру товара, у которого хотите изменить наименование")
+    except:
+        update.message.reply_text(
+            "Что-то пошло не так 3...")
+
+
+# function to handle the /help command
+def help(update, context):
+    update.message.reply_text('help command received')
+
+
+# function to handle errors occured in the dispatcher
+def error(update, context):
+    update.message.reply_text('an error occured')
+
+
+# function to handle normal text
+def text(update, context):
+    if context.user_data['state'] == ENTER_API_KEY:
+        return received_api_key(update, context)
+
+    if context.user_data['state'] == ENTER_NM_ID:
+        return received_nm_id(update, context)
+
+    if context.user_data['state'] == ENTER_NEW_NAME:
+        return received_new_name(update, context)
+
+
+def main():
+    TOKEN = "5299391428:AAHs4zLVrFwvw4GfW6zNMIdnfVNG6884jn0"
+
+    # create the updater, that will automatically create also a dispatcher and a queue to
+    # make them dialogue
+    updater = Updater(TOKEN, use_context=True)
+    dispatcher = updater.dispatcher
+
+    # add handlers for start and help commands
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("help", help))
+
+    # add an handler for normal text (not commands)
+    dispatcher.add_handler(MessageHandler(Filters.text, text))
+
+    # add an handler for errors
+    dispatcher.add_error_handler(error)
+
+    # start your shiny new bot
+    updater.start_polling()
+
+    # run the bot until Ctrl-C
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
---
+++ wb.py
@@ -0,0 +1,103 @@
+import requests
+import json
+
+
+headers = {"Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NJRCI6Ijk5NWJiZTY3LWQyMGItNDU3Mi04NmFiLWQ0NmVlMWUyNzFjMyJ9.3f5juiWxnbFFN1VKFsERRIjpXR7BkgxMlYLQv2oGHfw"}
+
+# headers['Authorization'] += '1'
+info = requests.get('https://suppliers-api.wildberries.ru/api/v1/directory/wbsizes', headers=headers)
+print()
+
+
+def search_data(nm_id):
+    data = {
+        "id": "1",
+        "jsonrpc": "2.0",
+        "params": {
+            "filter": {
+                # Поиск в соответсвии с условиями запроса
+                "find": [
+                    {
+                        "column": "nomenclatures.nmId",  # поиск по NM id
+                        "search": nm_id  # значение типа int
+                    }
+                ],
+                # Сортировка по указанному полю в указанном порядке.
+                "order": {
+                    "column": "createdAt",
+                    "order": "asc"
+                }
+            },
+            # Пагинация. Вывести максимум 10 карточек после пропуска 20. Выведутся карточки с 20 по 30 из тех, которые соотетствуют фильтру.
+            "query": {
+                "limit": 10,
+                "offset": 0
+            },
+            # "withError": false  # параметр, указывающий, что вернутся только карточки, в которых есть ошибки, которые не удалось создать. Параметр не обязательный, если его не указывать, вернутся только созданные карточки.
+        }
+    }
+    return data
+
+
+def get_card_info(data, headers):
+    info = requests.post('https://suppliers-api.wildberries.ru/card/list', json=data, headers=headers)
+
+    return info
+
+
+def prepare_card_info(info, headers):
+    info = json.loads(info.content.decode('utf8'))  # Делаем json
+    info = info['result']['cards'][0]
+    imt_id = info['imtId']
+    supplier_id = info['supplierId']
+    data = {
+        "id": info['id'],
+        "jsonrpc": "2.0",
+        "params": {
+            "imtID": imt_id,
+            "supplierID": supplier_id
+        }
+    }
+    info = requests.post('https://suppliers-api.wildberries.ru/card/cardByImtID', json=data, headers=headers)
+    info = info.text
+    info = json.loads(info)['result']
+    return info
+
+
+def info_to_upload(info, new_product_name):
+    info['card']['addin'][1]['params'][0]['value'] = new_product_name
+    updated_info = dict()
+    updated_info['params'] = {'card': {}}
+
+    card_params = ['id', 'imtId', 'userId', 'supplierId', 'imtSupplierId', 'object', 'parent', 'countryProduction',
+                   'addin', 'nomenclatures']
+    for param in card_params:
+        updated_info['params']['card'][param] = info['card'][param]
+
+    updated_info['jsonrpc'] = "2.0"
+    updated_info['id'] = info['card']['nomenclatures'][0]['id']
+    return updated_info
+
+data = search_data(nm_id=78914388)
+info = get_card_info(data, headers)
+info = prepare_card_info(info, headers)
+
+updated_info = info_to_upload(info, 'Лента для рукоделия ажурная')
+
+updater = requests.post('https://suppliers-api.wildberries.ru/card/update', json=updated_info, headers=headers)
+
+new_info = requests.post('https://suppliers-api.wildberries.ru/card/list', json=data, headers=headers)
+new_info = json.loads(new_info.content.decode('utf8'))  # Делаем json
+new_info = new_info['result']['cards'][0]
+print()
+
+# data_2 = search_data(nm_id=78850671)
+# info_2 = get_card_info(data_2, headers)
+# info_2 = prepare_card_info(info_2)
+# updated_info_2 = info_to_upload(info_2, 'Спортивный пистолет')
+# updater_2 = requests.post('https://suppliers-api.wildberries.ru/card/update', json=updated_info_2, headers=headers)
+# new_info_2 = requests.post('https://suppliers-api.wildberries.ru/card/list', json=data_2, headers=headers)
+# new_info_2 = json.loads(new_info_2.content.decode('utf8'))  # Делаем json
+#
+# print(updated_info)
+# print(updater)
